f9a2349ff229f940a70de041dc7c4c557590fb25
medhavisahgal <112403556+medhavisahgal@users.noreply.github.com>
2024-07-05 18:37:07 +0530

    Updated end

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let curMoleTile;
+let curBombTile;
+let gameOver = false;
 window.onload = function () {
     const cursor = document.querySelector(".cursor");
     window.addEventListener("mousemove", (e) => {
@@ -15,20 +17,24 @@ window.onload = function () {
 }
 let score = 0;
 function setgame() {
-    for (let i = 0; i < 9; i++) {``
+    for (let i = 0; i < 9; i++) {
+        ``
         let tile = document.createElement("div");
         tile.id = i.toString();
+        tile.addEventListener("click", selectTile);
         tile.classList.add("tile");
         tile.addEventListener("click", selectTile);
         document.getElementById("board").appendChild(tile);
     }
     setInterval(setMole, 1000);
+    setInterval(setBomb, 2000);
 }
 function getrandomtile() {
     let rantile = Math.floor(Math.random() * 9);
     return rantile.toString();
 }
 function setMole() {
+    if (gameOver) return;
     if (curMoleTile) {
         curMoleTile.innerHTML = "";
     }
@@ -36,17 +42,49 @@ function setMole() {
     mole.src = "./mole.png";
     mole.addEventListener("click", playwhackSound);
     let randomTile = getrandomtile();
+    if (curBombTile && curBombTile.id === randomTile) {
+        return;
+    }
     curMoleTile = document.getElementById(randomTile);
     curMoleTile.appendChild(mole);
 }
+function setBomb() {
+    if (gameOver) return;
+    if (curBombTile) {
+        curBombTile.innerHTML = "";
+    }
+    let bomb = document.createElement("img");
+    bomb.src = "./bomb.png";
+    bomb.addEventListener("click", playBombSound);
+    let randomTile = getrandomtile();
+    if (curMoleTile && curMoleTile.id === randomTile) {
+        return;
+    }
+    curBombTile = document.getElementById(randomTile);
+    curBombTile.appendChild(bomb);
+}
 function selectTile() {
+    if (gameOver) return;
     if (this === curMoleTile) {
         score += 10;
-        document.getElementById("score").innerText = score.toString();
+        document.getElementById("score").innerText = " Score : " + score.toString();
         playwhackSound();
     }
+    else if (this === curBombTile) {
+        document.getElementById("score").innerText = "Game Over : " + score.toString();
+        gameOver = true;
+        playBombSound();
+    }
 }
+
+
 function playwhackSound() {
+    if (gameOver) return;
     let whackSound = document.getElementById('whackSound');
     whackSound.play();
+}
+function playBombSound() {
+    if (gameOver) return;
+    let bombSound = document.getElementById('BombSound');
+    bombSound.play();
 }
